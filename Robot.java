4a0fb15cf1f82a42a518290828abb81f1873be1b
UVGJoseMart96 <mar15163@uvg.edu.gt>
2016-08-10 13:57:47 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -4,9 +4,16 @@
  */
 public class Robot {
     
-    private int posicion1;
-    private int posicion2;
+    private int posx;
+    private int posy;
+    private int direccion; //1 es norte, 2 es oeste, 3 es sur, 4 es este.
     
+    public Robot(int posx, int posy, int direccion){
+        this.posx = posx;
+        this.posy = posy;
+        this.direccion = direccion;
+    }
+
     public void analizar() {
         
     }
@@ -25,6 +32,64 @@ public class Robot {
     
     public void moverSur() {
         
+    }
+
+    @Override
+    public boolean libreAdelante(String[][] lab) {
+        boolean libre = false;
+        if (direccion == 1){
+            if (lab[posy - 1][posx].equals(" ")){
+                libre = true;
+            } else {
+                libre = false;
+            }
+        } else if (direccion == 2){
+            if (lab[posy][posx - 1].equals(" ")){
+                libre = true;
+            } else {
+                libre = false;
+            }
+        } else if (direccion == 3){
+            if (lab[posy + 1][posx].equals(" ")){
+                libre = true;
+            } else {
+                libre = false;
+            }
+        } else if (direccion ==4){
+            if (lab[posy][posx + 1].equals(" ")){
+                libre = true;
+            } else {
+                libre = false;
+            }
+        }
+        
+        return libre;
+    }
+
+    @Override
+    public boolean libreDerecha(String[][] lab) {
+        if (direccion == 1){
+            
+        } else if (direccion == 2){
+            
+        } else if (direccion == 3){
+            
+        } else if (direccion ==4){
+            
+        }
+    }
+
+    @Override
+    public boolean libreIzquierda(String[][] lab) {
+        if (direccion == 1){
+            
+        } else if (direccion == 2){
+            
+        } else if (direccion == 3){
+            
+        } else if (direccion ==4){
+            
+        }
     }
     
 }
