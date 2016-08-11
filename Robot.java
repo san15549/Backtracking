44d61d9e063f1f152cc9bececc6a61c78842e326
san15549 <san15549@uvg.edu.gt>
2016-08-10 22:49:38 -0600

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -7,38 +7,76 @@ public class Robot {
     private int posx;
     private int posy;
     private int direccion; //1 es norte, 2 es oeste, 3 es sur, 4 es este.
+    private CaminoP memoria;
+    private String[][] lab;
     
-    public Robot(int posx, int posy, int direccion){
+    public Robot(int posx, int posy, int direccion, String[][] lab){
         this.posx = posx;
         this.posy = posy;
         this.direccion = direccion;
-    }
-
-    public void analizar() {
-        
+        this.lab = lab;
+        memoria = new CaminoP();
     }
     
+    public boolean[] analizar(){
+        boolean libreN = false;
+        boolean libreO = false;
+        boolean libreS = false;
+        boolean libreE = false;
+        if (lab[posy + 1][posx].equals(" ")){
+           libreN = true;  
+        }
+        if (lab[posy - 1][posx].equals(" ")){
+            libreS = true;
+        }
+        if (lab[posy][posx + 1].equals(" ")){
+            libreE = true;
+        }
+        if (lab[posy][posx - 1].equals(" ")){
+            libreO = true;
+        }
+        boolean[] libre = new boolean[] {libreN,libreO,libreS,libreE};
+        return libre;
+    }
+
     public void moverNorte() {
-        
+        boolean[] libre = analizar();
+        if (libre[1]==true){
+            posy++;
+            lab[posy][posx] = "X";
+        }
     }
     
     public void moverOeste() {
-       
+       boolean[] libre = analizar();
+        if (libre[2]==true){
+            posx--;
+            lab[posy][posx] = "X";
+        }
     }
 
     public void moverEste() {
-        
+        boolean[] libre = analizar();
+        if (libre[4]==true){
+            posx++;
+            lab[posy][posx] = "X";
+        }
     }
     
     public void moverSur() {
-        
+        boolean[] libre = analizar();
+        if (libre[3]==true){
+            posy--;
+            lab[posy][posx] = "X";
+        }
     }
+    
 
-    @Override
-    public boolean libreAdelante(String[][] lab) {
+/*PARA ROBOT*/
+    public boolean libreAdelante() {
         boolean libre = false;
         if (direccion == 1){
-            if (lab[posy + 1][posx].equals(" ")){
+            if (lab[posy - 1][posx].equals(" ")){
                 libre = true;
             } else {
                 libre = false;
@@ -56,17 +94,15 @@ public class Robot {
                 libre = false;
             }
         } else if (direccion == 4){
-            if (lab[posy][posx - 1].equals(" ")){
+            if (lab[posy][posx + 1].equals(" ")){
                 libre = true;
             } else {
                 libre = false;
             }
         }
-        
         return libre;
     }
-
-    @Override
+/*PARA ROBOT*/
     public boolean libreDerecha(String[][] lab) {
         boolean libre = false;
     if (direccion == 4){
@@ -96,8 +132,7 @@ public class Robot {
     }
     return libre;
     }
-
-    @Override
+/*PARA ROBOT*/
     public boolean libreIzquierda(String[][] lab) {
         boolean libre = false;
     if (direccion == 2){
@@ -127,5 +162,11 @@ public class Robot {
     }
     return libre;
     }
-    
+
+    /*Metodo para Solucionar*/
+    public String solve(){
+        
+        
+        return "Success";
+    }
 }
